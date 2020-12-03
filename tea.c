0e4539573bdb23d9df15cdc02f31002aa2cf5adc
ringzer0dev <ringzer0dev@gmail.com>
2020-12-03 16:31:50 +0000

    Today's learning topic is done

--- tea.c
+++ tea.c
@@ -3,7 +3,6 @@
  * Coded by: ringzer0dev
  * Originally designed by Wheeler and Needham, 1994 
  */
-
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
@@ -26,4 +25,45 @@ void encrypt(uint64_t *v) {
     }
     v[0] = v0;
     v[1] = v1;
+}
+
+
+void decrypt(uint64_t *v) {
+    uint64_t v0 = v[0], v1 = v[1], sum = 0x2468af130eca8640, i; // set up
+    uint64_t delta = 0x123457898765432;                         // a key schedule constant
+    for (i = 0; i < 32; i++) {
+        v1 -= ((v0 << 4) + KEY[2]) ^ (v0 + sum) ^ ((v0 >> 5) + KEY[3]);
+        v0 -= ((v1 << 4) + KEY[0]) ^ (v1 + sum) ^ ((v1 >> 5) + KEY[1]);
+        sum -= delta;
+    } // end cycle
+    v[0] = v0;
+    v[1] = v1;
+}
+
+int main() {
+    uint64_t v[] = {0xFFFFFFFFDEADBEEF, 0xFFFFFFFFDEADBEEF};
+    // 64 bit ASCII->HEX translation of LJBLKECB: 4C4A424C4B454342
+    // 64 bit ASCII->HEX trnaslation of 0CELCIMF: 3043454C43494D46
+
+    //delta = 0x1234567898765432 = (1311768467425612850) base 10
+    //sum   = 0x468acf130eca8640 = (5083102810200507968) base 10
+    //printf("%"PRIu64"\n", sum);
+
+    //PRIx64 for hex
+    printf(" Original Values: [ ");
+    printf("%" PRIx64 " ", v[0]);
+    printf("%" PRIx64 " ] \n", v[1]);
+
+    encrypt(v);
+    printf(" Encrypted:       [ ");
+    printf("%" PRIx64 " ", v[0]);
+    printf("%" PRIx64 " ] \n", v[1]);
+
+    decrypt(v);
+    printf(" Decrypted:       [ ");
+    printf("%" PRIx64 " ", v[0]);
+    printf("%" PRIx64 " ] \n", v[1]);
+    printf("\n");
+
+    return 0;
 }
